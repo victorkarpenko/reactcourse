e14f416223d78d48493f7fc5729fa36bb36ff5c0
vikarpenko <Viktor_Karpenko@govitall.com>
2019-07-19 12:33:58 +0300

    init commit

---
+++ src/components/list/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const List = () => {
+    return(
+        <ul>
+            <li>test</li>
+            <li>test</li>
+            <li>test2</li>
+            <li>test</li>
+            <li>test</li>
+        </ul>
+    );
+};
+
+export default List;
