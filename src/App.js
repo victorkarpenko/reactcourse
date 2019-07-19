7044aaef7975f4a2c70b841626117cb76dbf755a
vikarpenko <Viktor_Karpenko@govitall.com>
2019-07-19 15:51:33 +0300

    Lesson-10 finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import List from './components/list'
-import Header from './components/header'
 import './App.css';
+import Header from "./components/header";
 
 const App = () => {
   return (
