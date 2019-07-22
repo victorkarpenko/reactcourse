a8a6b4ca151681860922bd60cd7925b363ed95db
vikarpenko <Viktor_Karpenko@govitall.com>
2019-07-22 09:50:50 +0300

    Lesson 11 complete

--- src/App.js
+++ src/App.js
@@ -3,12 +3,35 @@ import logo from './logo.svg';
 import List from './components/list'
 import './App.css';
 import Header from "./components/header";
+import Footer from "./components/footer";
 
 const App = () => {
   return (
     <div className="App">
-      <Header/>
+
+      {/*<Header/>
       <List/>
+      <Footer/>*/}
+      <header className="header">
+          <img src={logo} alt="" className="logo"/>
+      </header>
+        <aside className="sidebar">
+            <nav className="navigation">
+                <a href="/">home</a>
+                <a href="/order">order</a>
+                <a href="/blog">blog</a>
+            </nav>
+        </aside>
+        <main className="main">
+            <div className="main__img"></div>
+            <div className="main__profile"></div>
+            <div className="main__posts">
+                <div className="main__post-input"></div>
+                <div className="main__posts-list">
+                    <div className="post"></div>
+                </div>
+            </div>
+        </main>
     </div>
   );
 };
---
+++ src/components/footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <footer>
+            footer - react app
+        </footer>
+    )
+};
+
+export default Footer;
