cdddef8d3bb1d750a8879e55e792153a2a49abad
vikarpenko <Viktor_Karpenko@govitall.com>
2019-07-22 10:46:23 +0300

    Lesson 12 completed

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import List from './components/list'
+
 import './App.css';
 import Header from "./components/header";
-import Footer from "./components/footer";
+import Navbar from "./components/navbar";
+import Profile from "./components/profile";
 
 const App = () => {
   return (
     <div className="App">
 
-      {/*<Header/>
-      <List/>
-      <Footer/>*/}
-      <header className="header">
-          <img src={logo} alt="" className="logo"/>
-      </header>
-        <aside className="sidebar">
-            <nav className="navigation">
-                <a href="/">home</a>
-                <a href="/order">order</a>
-                <a href="/blog">blog</a>
-            </nav>
-        </aside>
-        <main className="main">
-            <div className="main__img"></div>
-            <div className="main__profile"></div>
-            <div className="main__posts">
-                <div className="main__post-input"></div>
-                <div className="main__posts-list">
-                    <div className="post"></div>
-                </div>
-            </div>
-        </main>
+      <Header/>
+      <Navbar/>
+      <Profile/>
+
+
+
     </div>
   );
 };
---
+++ src/components/header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../../logo.svg";
+
+const Header = () => {
+    return(
+        <header className="header">
+            <img src={logo} alt="" className="logo"/>
+        </header>
+    )
+};
+
+export default Header;
