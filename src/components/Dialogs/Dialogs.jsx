a74f683540a9554f31371d7fba07e61e770fdcef
vikarpenko <Viktor_Karpenko@govitall.com>
2019-07-23 11:53:23 +0300

    Lesson 22 complete, dialogsList, meassageList

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,55 @@
 import React from 'react';
 import c from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return(
         <div className={c.dialogs}>
-            dialogs page
+            <div className={c.usersList}>
+                <NavLink to='/dialogs/1' className={c.user}>
+                    <div className={c.avatar}>
+
+                    </div>
+                    <div className={c.userName}>
+                        Viktor
+                    </div>
+                </NavLink>
+
+                <NavLink to='/dialogs/2' className={c.user}>
+                    <div className={c.avatar}>
+
+                    </div>
+                    <div className={c.userName}>
+                        Cat Vasyl
+                    </div>
+                </NavLink>
+
+                <NavLink  to='/dialogs/3' className={c.user}>
+                    <div className={c.avatar}>
+
+                    </div>
+                    <div className={c.userName}>
+                       Dog Sebek
+                    </div>
+                </NavLink>
+
+                <NavLink  to='/dialogs/4' className={c.user}>
+                    <div className={c.avatar}>
+
+                    </div>
+                    <div className={c.userName}>
+                        Sergey 01k
+                    </div>
+                </NavLink>
+            </div>
+            <div className={c.dialogsList}>
+                <div className={c.dialog}>
+                    <div className={c.inputMsg + " " + c.msg}>Hey, how are you</div>
+                    <div className={c.outputMsg + " " + c.msg}>Hi, a'im zbs. You?</div>
+                    <div className={c.outputMsg + " " + c.msg}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam beatae blanditiis eaque explicabo illum itaque iure laboriosam libero modi, odio quisquam quos repellendus repudiandae rerum sapiente suscipit tempora tempore voluptates?</div>
+                    <div className={c.inputMsg + " " + c.msg}>Okay, thanks for you respond</div>
+                </div>
+            </div>
         </div>
     )
 };
