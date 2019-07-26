ddb0cce16f6e3a8c0192d177a404cea039da458d
vikarpenko <Viktor_Karpenko@govitall.com>
2019-07-26 13:01:45 +0300

    test gh-pages

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.309d61803e80cce32d7cfdd59767ee97.js"
+  "/reactcourse/precache-manifest.309d61803e80cce32d7cfdd59767ee97.js"
 );
 
 self.addEventListener('message', (event) => {
