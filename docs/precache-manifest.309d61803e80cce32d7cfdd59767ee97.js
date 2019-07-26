b5f67d00855f4d0dff1f7e802c1f3909162de4e8
vikarpenko <Viktor_Karpenko@govitall.com>
2019-07-26 13:03:17 +0300

    test gh-pages

--- docs/precache-manifest.309d61803e80cce32d7cfdd59767ee97.js
+++ docs/precache-manifest.309d61803e80cce32d7cfdd59767ee97.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "5a2f944334bce8df2ab80a5ab5ad0544",
-    "url": "/index.html"
+    "url": "/reactcourse/index.html"
   },
   {
     "revision": "7467c9854cd6f8cf76b8",
-    "url": "/static/css/main.7bab868d.chunk.css"
+    "url": "/reactcourse/static/css/main.7bab868d.chunk.css"
   },
   {
     "revision": "34e6aa2dfe4db0397b2f",
-    "url": "/static/js/2.1f9dfcc4.chunk.js"
+    "url": "/reactcourse/static/js/2.1f9dfcc4.chunk.js"
   },
   {
     "revision": "7467c9854cd6f8cf76b8",
-    "url": "/static/js/main.98c65566.chunk.js"
+    "url": "/reactcourse/static/js/main.98c65566.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    "url": "/reactcourse/static/js/runtime~main.a8a9905a.js"
   },
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/static/media/logo.5d5d9eef.svg"
+    "url": "/reactcourse/static/media/logo.5d5d9eef.svg"
   }
 ]);
