7398873b6efd5ed8c6c42b60e05358e77b29fad7
vikarpenko <Viktor_Karpenko@govitall.com>
2019-08-21 09:22:21 +0300

    Lesson 51 complete: users from API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,54 +1,13 @@
 import React from 'react';
 import c from './Users.module.css';
+import * as axios from "axios";
+import avatar from '../../assets/images/avatar.png';
 
 let Users = (props) =>{
     if(props.users.length === 0){
-        props.setUsers([
-            {
-                id: 1,
-                fullName: 'Viktor K',
-                avatarUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1024px-Circle-icons-profile.svg.png',
-                status: 'Lorem ipsum lolsd fds fds fds',
-                location: {
-                    city: 'Kyiv',
-                    country: 'Ukraine',
-                },
-                followed: false
-            },
-            {
-                id: 2,
-                fullName: 'Sebek K',
-                avatarUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1024px-Circle-icons-profile.svg.png',
-                status: 'Lorem ipsum lolsd fds fds fds',
-                location: {
-                    city: 'Minsk',
-                    country: 'Belarus',
-                },
-                followed: false
-            },
-            {
-                id: 3,
-                fullName: 'Cotlovan K',
-                avatarUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1024px-Circle-icons-profile.svg.png',
-                status: 'Lorem ipsum lolsd fds fds fds',
-                location: {
-                    city: 'Kharkov',
-                    country: 'Ukraine',
-                },
-                followed: true
-            },
-            {
-                id: 4,
-                fullName: 'Serega K',
-                avatarUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1024px-Circle-icons-profile.svg.png',
-                status: 'Lorem ipsum lolsd fds fds fds',
-                location: {
-                    city: 'Kyiv',
-                    country: 'Ukraine',
-                },
-                followed: true
-            },
-        ]);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items)
+        });
     }
 
   return(
@@ -56,15 +15,15 @@ let Users = (props) =>{
           {
               props.users.map(u => <div className={c.user} key={u.id}>
                   <div className={c.user__left}>
-                      <img className={c.user__avatar} src={u.avatarUrl} alt=""/>
+                      <img className={c.user__avatar} src={u.photos.small !== null ? u.photos.small : avatar} alt=""/>
                       {u.followed ?  <button onClick={()=>{props.unfollow(u.id)}} className={c.user__button}>Unfollow</button> :  <button onClick={()=>{props.follow(u.id)}} className={c.user__button}>Follow</button>}
                   </div>
                   <div className={c.user__center}>
-                      <div className={c.user__fullname}>{u.fullName}</div>
+                      <div className={c.user__fullname}>{u.name}</div>
                       <div className={c.user__status}>{u.status}</div>
                   </div>
                   <div className={c.user__right}>
-                      {u.location.country}, {u.location.city}
+                      {/*u.location.country}, {u.location.city*/}
                   </div>
               </div>)
           }
