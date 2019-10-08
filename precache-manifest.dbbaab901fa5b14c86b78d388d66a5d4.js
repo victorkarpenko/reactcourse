0b47b9be889ec9cbce6d787d0873c945672eb862
vikarpenko <Viktor_Karpenko@govitall.com>
2019-10-08 09:54:58 +0300

    Updates

---
+++ precache-manifest.dbbaab901fa5b14c86b78d388d66a5d4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03cebe925f4561c5e5ef8e629797e089",
+    "url": "/react-social-network/index.html"
+  },
+  {
+    "revision": "06d41eaa5b73f60c444a",
+    "url": "/react-social-network/static/css/3.bbda7c01.chunk.css"
+  },
+  {
+    "revision": "bd083777bb3c86c065c3",
+    "url": "/react-social-network/static/css/main.6b9ac613.chunk.css"
+  },
+  {
+    "revision": "3f71238b8bf8a2abe745",
+    "url": "/react-social-network/static/js/2.54a42f4d.chunk.js"
+  },
+  {
+    "revision": "06d41eaa5b73f60c444a",
+    "url": "/react-social-network/static/js/3.fef82df1.chunk.js"
+  },
+  {
+    "revision": "bd083777bb3c86c065c3",
+    "url": "/react-social-network/static/js/main.99d0c7ba.chunk.js"
+  },
+  {
+    "revision": "f851c549d2ddc412c1db",
+    "url": "/react-social-network/static/js/runtime~main.1742de81.js"
+  },
+  {
+    "revision": "0d09cb21328220cbafa40e8886c76663",
+    "url": "/react-social-network/static/media/avatar.0d09cb21.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-social-network/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dbe1e94c64837f2c6b9ad358bccb0131",
+    "url": "/react-social-network/static/media/logout.dbe1e94c.svg"
+  },
+  {
+    "revision": "8726358776dc8f0cc35c8aa570349e91",
+    "url": "/react-social-network/static/media/lookingJobIcon.87263587.png"
+  },
+  {
+    "revision": "a8ed85cb9ed5b8bd9be5009050d65274",
+    "url": "/react-social-network/static/media/preloader.a8ed85cb.gif"
+  }
+]);
